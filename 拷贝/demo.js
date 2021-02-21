2ee9737246cfb98bcf4f7e14e7a96cae7763b58e
ZSX-DB <Sx82199609124535@outlook.com>
2021-02-22 00:26:57 +0800

    update

--- 拷贝/demo.js
+++ 拷贝/demo.js
@@ -2,6 +2,18 @@
  * 深拷贝
  */
 
+// 解决循环引用问题: 额外开辟一个存储空间，来存储当前对象和拷贝对象的对应关系，当需要拷贝当前对象时，先去存储空间中找，有没有拷贝过这个对象
+const clone = (target, wm = new WeakMap()) => {
+    if (typeof target !== 'object') return target
+    let result = target instanceof Array ? [] : {}
+    if (wm.get(target)) return wm.get(target)
+    wm.set(target, result)
+    for (const key in target) {
+        if (target.hasOwnProperty(key)) result[key] = clone(target[key], wm)
+    }
+    return result
+}
+
 // 循环引用对象
 const circularObj = {
     a: 1,
@@ -15,45 +27,17 @@ const circularObj = {
 }
 circularObj.i = circularObj
 
+const obj = clone(circularObj)
+obj.j = 'J'
+console.log(obj, circularObj)
+
+
 // 循环引用数组
 const circularArr = [
-    1, true, 'str', () => 'D'
+    1, true, 'str', () => 'D', {a: 97}, [false, 'f', 99]
 ]
 circularArr.push(circularArr)
 
-// 解决办法: 额外开辟一个存储空间，来存储当前对象和拷贝对象的对应关系，当需要拷贝当前对象时，先去存储空间中找，有没有拷贝过这个对象
-const clone = (target, map = new Map()) => {
-    if (typeof target !== 'object') return target
-    let result = target instanceof Array ? [] : {}
-    for (const prop in target) {
-        if (target.hasOwnProperty(prop) && typeof target[prop] === 'object') {
-            console.log(prop, map.get(target))
-            if (map.get(target)) return map.get(target)
-            map.set(target, result)
-            result[prop] = clone(target[prop], map)
-        } else {
-            result[prop] = target[prop]
-        }
-    }
-    return result
-}
-
-let obj = {
-    a: 3,
-    b: [3,5],
-    c:{
-        d: false
-    }
-}
-// obj.e = obj
-let s = clone(obj)
-
-console.log(s)
-
-// const obj = clone(circularObj)
-// obj.f = 'F'
-// console.log(obj, circularObj)
-
-// const arr = clone(circularArr)
-// arr.push('OK')
-// console.log(arr, circularArr)
+const arr = clone(circularArr)
+arr.push('OK')
+console.log(arr, circularArr)
