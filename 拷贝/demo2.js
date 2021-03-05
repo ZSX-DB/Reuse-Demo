fdbf01b0a02fb3281edf8cde14403119183f2e23
ZSX-DB <Sx82199609124535@outlook.com>
2021-03-05 13:41:33 +0800

    update

--- 拷贝/demo2.js
+++ 拷贝/demo2.js
@@ -3,61 +3,60 @@
  * @param target
  * @returns {{}|[]|*}
  */
-const clone = target => {
-    const getType = target => {
-        if (typeof target !== "object") return 1
-        return target instanceof Array ? 2 : 3
-    }
-
-    // 循环引用标记
-    const wm = new WeakMap()
-    wm.set(target, 1)
-
-    const cloneArr = arr => {
-        const res = []
-        for (const a of arr) {
-            switch (getType(a)) {
-                case 1:
-                    res.push(a)
-                    break
-                case 2:
-                    wm.get(a) ? res.push(res) : res.push(cloneArr(a))
-                    break
-                case 3:
-                    wm.get(a) ? res.push(res) : res.push(cloneObj(a))
-            }
-        }
-        return res
-    }
-
-    const cloneObj = obj => {
-        const res = {}
-        for(const [k, v] of Object.entries(obj)){
-            switch (getType(v)) {
-                case 1:
-                    res[k] = v
-                    break
-                case 2:
-                    wm.get(v) ? res[k] = res : res[k] = cloneArr(v)
-                    break
-                case 3:
-                    wm.get(v) ? res[k] = res : res[k] = cloneObj(v)
-            }
-        }
-        return res
-    }
-
-    switch (getType(target)) {
-        case 1:
-            return target
-        case 2:
-            return cloneArr(target)
-        case 3:
-            return cloneObj(target)
-    }
-
-}
-
+// const clone = target => {
+//     const getType = target => {
+//         if (typeof target !== "object") return 1
+//         return target instanceof Array ? 2 : 3
+//     }
+//
+//     // 循环引用标记
+//     const wm = new WeakMap()
+//     wm.set(target, 1)
+//
+//     const cloneArr = arr => {
+//         const res = []
+//         for (const a of arr) {
+//             switch (getType(a)) {
+//                 case 1:
+//                     res.push(a)
+//                     break
+//                 case 2:
+//                     wm.get(a) ? res.push(res) : res.push(cloneArr(a))
+//                     break
+//                 case 3:
+//                     wm.get(a) ? res.push(res) : res.push(cloneObj(a))
+//             }
+//         }
+//         return res
+//     }
+//
+//     const cloneObj = obj => {
+//         const res = {}
+//         for(const [k, v] of Object.entries(obj)){
+//             switch (getType(v)) {
+//                 case 1:
+//                     res[k] = v
+//                     break
+//                 case 2:
+//                     wm.get(v) ? res[k] = res : res[k] = cloneArr(v)
+//                     break
+//                 case 3:
+//                     wm.get(v) ? res[k] = res : res[k] = cloneObj(v)
+//             }
+//         }
+//         return res
+//     }
+//
+//     switch (getType(target)) {
+//         case 1:
+//             return target
+//         case 2:
+//             return cloneArr(target)
+//         case 3:
+//             return cloneObj(target)
+//     }
+//
+// }
 
 let obj = {
     a: 3,
@@ -69,7 +68,8 @@ let obj = {
             g: 'str'
         }
     },
-    h: [1, 3, 5, 7, 9]
+    h: [1, 3, 5, 7, 9],
+    dd: null
 }
 obj.i = obj
 
