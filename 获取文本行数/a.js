439ae61cd97137721cc908a56fe94c983ce00cb9
ZSX-DB <Sx82199609124535@outlook.com>
2021-05-23 01:39:25 +0800

    update

--- 获取文本行数/a.js
+++
@@ -1,7 +0,0 @@
-let str1 = '85px'
-let str2 = '92pt'
-let str3 = '34ss5kk6'
-let reg = /[^0-9.]/g
-console.log(str1.replace(reg, ''))
-console.log(str2.replace(reg, ''))
-console.log(str3.replace(reg, ''))
