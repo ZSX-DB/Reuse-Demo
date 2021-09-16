d46df6df3c17ce70d3fd44f8c50c447083ec777e
ZSX-DB <50871520+ZSX-DB@users.noreply.github.com>
2021-09-17 01:33:01 +0800

    update

--- 正则表达式/numInStr.js
+++ 正则表达式/numInStr.js
@@ -2,8 +2,54 @@
  * 获取字符串中的数字
  */
 
-let str = '规格：207ml,价格100元'
+const str = '规格：207 ml，价格 100.00 元'
 
-// 两种方式均不会对原字符串造成影响
 console.log(str.match(/\d+/g))
-console.log(str.replace(/[^0-9]/ig, ''))
+console.log(str.replace(/[^0-9.]/g, ''))
+
+// 判断是否为数字并且最多两位小数
+console.log(/^\d+\.\d{0,2}$/.test('14.6'))
+console.log(/^\d+\.\d{0,2}$/.test('142.643'))
+
+/**
+ * @param str {string}
+ * @returns {string}
+ */
+const getPureNum = str => str.replace(/[^0-9]/g, '')
+console.log('============== getPureNum ==============')
+console.log(getPureNum('S12.3'))
+console.log(getPureNum('www.456.com'))
+
+/**
+ * @param str {string}
+ * @returns {string}
+ */
+const getTwoDecimalsNum = str => {
+    if (str.includes('.') === false) {
+        return str.replace(/[^0-9]/g, '')
+    }
+    str = str.replace(/[^0-9.]/g, '')
+    while (str[0] === '.') {
+        str = str.substring(1, str.length)
+    }
+    if (str.includes('.') === false) {
+        return str
+    }
+    const firstDotIdx = str.indexOf('.')
+    const left = str.substring(0, firstDotIdx)
+    const right = str.substring(firstDotIdx, str.length).replace(/[.]/g, '').substring(0, 2)
+    return `${left}.${right}`
+}
+console.log('============== getPureNum ==============')
+console.log(getTwoDecimalsNum('S12.3'))
+console.log(getTwoDecimalsNum('www.456.com'))
+console.log(getTwoDecimalsNum('545.768'))
+console.log(getTwoDecimalsNum('443.7'))
+console.log(getTwoDecimalsNum('41.'))
+console.log(getTwoDecimalsNum('...2331..3'))
+console.log(getTwoDecimalsNum('...2341.'))
+console.log(getTwoDecimalsNum('...2351'))
+console.log(getTwoDecimalsNum('...2361...'))
+console.log(getTwoDecimalsNum('...2371...899'))
+
+
